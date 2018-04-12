61ea8afa466a9356d90bbe3a963141c18075a8dc
SamChang045 <willpower03@gmail.com>
2018-04-12 22:24:09 +0800

    add show page for public users

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,10 +6,13 @@ class ProductsController < ApplicationController
     @products = Product.page(params[:page]).per(9)
    end
   
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def add_to_cart
     @product = Product.find(params[:id])
     current_cart.add_cart_item(@product)
-
     redirect_to root_path
   end
   
