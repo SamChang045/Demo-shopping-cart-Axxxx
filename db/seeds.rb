9b481f8ff96e7fc866a0b228b45aa18629728b81
SamChang045 <willpower03@gmail.com>
2018-04-15 11:11:42 +0800

    setup seed data for default categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Category
+
+Category.destroy_all
+
+category_list = [
+  { name: "Books & Audible" },
+  { name: "Movies, Music & Games" },
+  { name: "Electronics, Computers & Office" },
+  { name: "Home, Garden, Pets & Tools" },
+  { name: "Food & Grocery" },
+  { name: "Beauty & Health" },
+  { name: "Toys, Kids & Baby" },
+  { name: "Clothing, Shoes & Jewelry" },
+  { name: "Handmade" },
+  { name: "Sports & Outdoors" }
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name] )
+end
+puts "Category created!"
