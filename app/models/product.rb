a06f94c94fef23049dc3554d0e6f52195923ff80
SamChang045 <willpower03@gmail.com>
2018-04-15 11:13:10 +0800

    link category and product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
   validates_presence_of :name, :description, :price
   mount_uploader :image, PhotoUploader
 
+  belongs_to :category
+
   has_many :cart_items, dependent: :destroy
   has_many :carts, through: :cart_items
   
@@ -12,5 +14,5 @@ class Product < ApplicationRecord
   def is_favorited?(user)
     self.favorited_users.include?(user)
   end
-  
+
 end
