32b4cc7351d89da2152bd6119b1debf3c7218317
SamChang045 <willpower03@gmail.com>
2018-04-15 11:25:46 +0800

    add category select option in produt index and show page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -31,7 +31,7 @@ class Admin::ProductsController < ApplicationController
   private 
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :image)
+    params.require(:product).permit(:name, :description, :price, :image, :category_id)
   end
 
 end
