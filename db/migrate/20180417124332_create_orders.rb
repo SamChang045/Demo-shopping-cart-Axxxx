5239a352e4717d9215e57bf07d029d7774a0e492
SamChang045 <willpower03@gmail.com>
2018-04-17 20:46:36 +0800

    create order_item model

--- db/migrate/20180417124332_create_orders.rb
+++ db/migrate/20180417124332_create_orders.rb
@@ -1,7 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
-
+      t.string  :name
+      t.string  :address
+      t.string  :payment_status
+      t.string  :shipping_status
+      t.string  :phone
+      t.integer :aomount
+      t.integer :sn 
+      t.integer :user_id, null: false
       t.timestamps
     end
   end
