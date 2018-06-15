25df862d8bd8dd89c19dd07001c8b21854580eef
SamChang045 <willpower03@gmail.com>
2018-06-15 16:42:28 +0800

    Fixed the NaN when the total value of items below zero.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,7 +24,7 @@ class OrdersController < ApplicationController
           order_item.save!
         end
         @order.save!  
-        UserMailer.notify_order_create(@order).deliver_now! #寄送訂單成立email
+        #UserMailer.notify_order_create(@order).deliver_now! #寄送訂單成立email
         session[:cart_id] = nil #訂單成立清空購物車
         redirect_to order_path(@order)
       else
